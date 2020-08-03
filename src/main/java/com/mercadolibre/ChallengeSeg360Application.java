e8ac221fa0d84dccca215444ca0384fcc5dbc1b4
katmilo0309 <katmilo0309@gmail.com>
2020-08-02 20:01:10 -0500

    first commit

---
+++ src/main/java/com/mercadolibre/ChallengeSeg360Application.java
@@ -0,0 +1,13 @@
+package com.mercadolibre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSeg360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeSeg360Application.class, args);
+	}
+
+}
